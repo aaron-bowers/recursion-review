8cf38b91073da8209a63c5f27c82858fc693e9cc
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-11 17:28:47 -0700

    Write code for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,57 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //if single value
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  //if array
+  if (Array.isArray(obj)) {
+
+    if (obj.length === 0) {
+      return '[]';
+    }
+
+    var jsonArray = '';
+    jsonArray += '[';
+
+    for (var i = 0; i < obj.length; i++) {
+      jsonArray += stringifyJSON(obj[i]);
+
+      if (i === obj.length - 1) {
+        jsonArray += ']';
+      } else {
+        jsonArray += ',';
+      }
+    }
+    return jsonArray;
+  }
+
+  //if obj
+  if (typeof obj === 'object') {
+
+    if (obj === undefined) {
+      return '{}';
+    }
+
+    var results = [];
+
+    for (var keys in obj) {
+      if ((keys !== 'function' && obj[keys] !== 'function') && (obj[keys] !== undefined && keys !== undefined)) {
+        results.push(stringifyJSON(keys) + ':' + stringifyJSON(obj[keys]));
+      }
+    }
+
+    return '{' + results.join(',') + '}';
+
+  }
 };
